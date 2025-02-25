8553103fa2d209fc6dcdbf80314c9fea219408d2
zakary2841 <26402356+zakary2841@users.noreply.github.com>
2025-02-25 21:54:56 +0000

    Update InAppNotifications.plugin.js
    Fixed errors with the confirmation modal  and changed hosted file location
    Fixed channel types error - might find a way to dynamically get this is future
    Fixed error with bots/webhooks causing errors as they have an undefined globalName

--- InAppNotifications/InAppNotifications.plugin.js
+++ InAppNotifications/InAppNotifications.plugin.js
@@ -152,7 +152,7 @@ const config = {
       }
 
       load() {
-        BdApi.showConfirmationModal(
+        BdApi.UI.showConfirmationModal(
           "Library plugin is needed",
           `The library plugin needed for AQWERT'sPluginBuilder is missing. Please click Download Now to install it.`,
           {
@@ -160,11 +160,11 @@ const config = {
             cancelText: "Cancel",
             onConfirm: () => {
               request.get(
-                "https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js",
+                "https://raw.githubusercontent.com/Zakary2841/BetterDiscordPlugins/main/InAppNotifications/InAppNotifications.plugin.js",
                 (error, response, body) => {
                   if (error)
                     return electron.shell.openExternal(
-                      "https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js"
+                      "https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/Zakary2841/BetterDiscordPlugins/main/InAppNotifications/InAppNotifications.plugin.js"
                     );
 
                   fs.writeFileSync(
@@ -206,7 +206,33 @@ const config = {
       } = DiscordModules;
       const { Webpack } = BdApi;
 
-      const ChannelTypes = WebpackModules.getByProps("GUILD_TEXT", "PUBLIC_THREAD") || {};
+
+          // Manually defining this for now
+      const ChannelTypes = {
+          GUILD_TEXT: 0,                // a text channel within a server
+          DM: 1,                        // a direct message between users
+          GUILD_VOICE: 2,               // a voice channel within a server
+          GROUP_DM: 3,                  // a direct message between multiple users
+          GUILD_CATEGORY: 4,            // an organizational category that contains up to 50 channels
+          GUILD_ANNOUNCEMENT: 5,        // a channel that users can follow and crosspost into their own server
+          ANNOUNCEMENT_THREAD: 10,      // a temporary sub-channel within a GUILD_ANNOUNCEMENT channel
+          PUBLIC_THREAD: 11,            // a temporary sub-channel within a GUILD_TEXT or GUILD_FORUM channel
+          PRIVATE_THREAD: 12,           // a temporary sub-channel within a GUILD_TEXT channel, viewable only by those invited
+          GUILD_STAGE_VOICE: 13,        // a voice channel for hosting events with an audience
+          GUILD_DIRECTORY: 14,          // the channel in a hub containing the listed servers
+          GUILD_FORUM: 15,              // Channel that can only contain threads
+          GUILD_MEDIA: 16               // Channel that can only contain threads, similar to GUILD_FORUM channels
+      };
+      // Create a reverse lookup object
+      const ChannelTypeNames = Object.fromEntries(
+      Object.entries(ChannelTypes).map(([name, value]) => [value, name])
+      );
+	  // Function to convert the number to the name
+	  function getChannelNameById(id) {
+	    return ChannelTypeNames[id] || 'Unknown';
+	  	  }
+	  
+      // const ChannelTypes = WebpackModules.getByProps("GUILD_TEXT", "PUBLIC_THREAD") || {};
 	  // const ChannelTypes = WebpackModules.getModule(m => m?.GUILD_TEXT !== undefined, { searchExports: true }) || {};
 	  // This should also work but going to test to see which is better.
       const MuteStore = WebpackModules.getByProps("isSuppressEveryoneEnabled");
@@ -591,7 +617,7 @@ const config = {
               e
             );
             BdApi.alert(
-              "InAppNotifications",Error_Message
+              "InAppNotifications", Error_Message
             );
           }
 
@@ -628,8 +654,7 @@ const config = {
                 );
               } catch {
                 BdApi.alert(
-                  "InAppNotifications",
-                  Error_Message
+                  "InAppNotifications", Error_Message
                 );
               }
             }
@@ -666,8 +691,7 @@ const config = {
                 );
               } catch {
                 BdApi.alert(
-                  "InAppNotifications",
-                  Error_Message
+                  "InAppNotifications", Error_Message
                 );
               }
             }
@@ -813,8 +837,8 @@ const config = {
           if (!this.supposedToNotify(message, channel) && !keywordFound) return;
           let authorString = "";
           let authorName = `${author.globalName} (${author.discriminator === "0" ? author.username : author.tag})`;
-          if (author.globalName === null || author.globalName.toLowerCase() === author.username.toLowerCase()) {
-            authorName = author.discriminator === "0" ? author.username : author.tag;
+          if (!author.globalName || author.globalName.toLowerCase() === author.username.toLowerCase()) {
+            authorName = author.discriminator === "0" || "0000" ? author.username : author.tag;
           }
           if (channel.guild_id) {
             const guild = GuildStore.getGuild(channel.guild_id);
